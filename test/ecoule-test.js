893e840afe49fb4a2222ca918b0851eea73bb602
Martin Gausby <martin@gausby.dk>
2013-10-11 00:06:22 +0200

    Added a simple integration test. This shows how the framework works and contains popular characters from Watership Down.

--- test/ecoule-test.js
+++ test/ecoule-test.js
@@ -12,4 +12,101 @@ var refute = buster.referee.refute;
 
 var basicConfig = {};
 
-buster.testCase('Ecoule Engine', {});
+buster.testCase('Ecoule Engine', {
+    'integration test: watership down edition': function (done) {
+        /* source =================================================== */
+        function Source (config) {
+            this.title = config.title || 'Untitled source';
+            this.data = config.data || [];
+        }
+
+        Source.prototype.refresh = function(done) {
+            var err; // undefined, no errors
+
+            return done(err, this.data);
+        };
+
+
+        /* data-handler ============================================= */
+        function DataHandler (config) {
+            this.match = config.match || {};
+        }
+
+        DataHandler.prototype.execute = function(entry, done) {
+            var err; // undefined, no errors
+
+            entry.name = entry.name.toUpperCase();
+
+            return done(err);
+        };
+
+
+        /* transformer ============================================== */
+        function Transformer (config) {
+            config = config || {};
+            this.queries = config.queries || {};
+            this.preprocessors = config.preprocessors || [];
+            this.postprocessors = config.postprocessors || [];
+            this.outputs = config.outputs || [];
+        }
+
+        Transformer.prototype.execute = function (done) {
+            var err; // undefined, no errors
+
+            // just pass the data from the queries along, untouched
+            return done(err, this.data);
+        };
+
+
+        /* output =================================================== */
+        function Output (config) {
+            config = config || {};
+        }
+
+        Output.prototype.execute = function (output, done) {
+            var err; // undefined, no errors
+
+            assert.equals(output, {
+                owsla: [{ name: "BIGWIG" }],
+                rabits: [{ name: "FIVER" }, { name: "BIGWIG" }]
+            });
+
+            done(err);
+        };
+
+
+        // Put it all together and try to execute it
+        var instance = new Ecoule({
+            'sources': [
+                new Source({
+                    title: 'test',
+                    data: [{ name: 'fiver' }, { name: 'bigwig' }]
+                })
+            ],
+            'data-handlers': [
+                new DataHandler({
+                    match: {
+                        name: { typeOf: 'string' }
+                    }
+                })
+            ],
+            'transformers': [
+                new Transformer ({
+                    queries: {
+                        owsla: { name: { equals: 'BIGWIG' }},
+                        rabits: { name: { typeOf: 'string' }}
+                    },
+                    outputs: [
+                        new Output({})
+                    ]
+                })
+            ]
+        });
+
+        instance.refresh(function(err) {
+            // this setup should not return errors
+            refute.isObject(err);
+            done();
+        });
+    }
+});
